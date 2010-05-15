a5076253c9a576ac0c1a0b4bcd78658ef3338488
Thomas Lee <thomas.lee@shinetech.com>
2010-05-15 19:48:29 +1000

    Fix an example.

--- examples/simple.js
+++ examples/simple.js
@@ -3,7 +3,7 @@ var sys = require("sys");
 var fs = require("fs");
 
 var data = fs.readFileSync(__dirname + "/simple.jazz");
-var template = jazz.compile(data, {"compiler:debug": true});
-template.eval({}, function(data) { sys.puts("narf?"); sys.puts(data); });
+var template = jazz.compile(data);
+template.eval({}, function(data) { sys.puts(data); });
 
 
